d0b0b2d58314e416cbb37e5df3595e53a8a178f6
zhuzhu <ilya.zhuzhukov@yandex.ru>
2024-05-30 14:15:22 +0300

    hash commit

--- my_scripts/git_hash_commit_checking.sh
+++ my_scripts/git_hash_commit_checking.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-SHORT_HASH_COMMIT=(chrootww hash 1.0.1 1 17971224)
+HASH_COMMIT=(chrootww hash 1.0.1 1 179wd71224)
 
-for i in ${SHORT_HASH_COMMIT[@]}; do
-    flag=0
+for i in ${HASH_COMMIT[@]}; do
+    hash_commit_exist=0
     if [ ${#i} != 8 ] && [ ${#i} != 40 ]; then
-        flag=1
+        hash_commit_exist=1
         continue
     else
         if HASH_COMMIT=$(git rev-parse $i 2>/dev/null); then
             echo $i is a git hash-commit.
 	        exit
         else
-            flag=1
+            hash_commit_exist=1
             continue
         fi
     fi
 done
 
-    if [ $flag == 1 ]; then
+    if [ $hash_commit_exist == 1 ]; then
 	    echo There is no hash in this tag.
     fi
+
+# for i in ${SHORT_HASH_COMMIT[@]}; do flag=0 && if [ ${#i} != 8 ] && [ ${#i} != 40 ]; then flag=1 && continue; else if HASH_COMMIT=$(git rev-parse $i 2>/dev/null); then echo $i is a git hash-commit. && exit; else flag=1 && continue; fi; fi; done; if [ $flag == 1 ]; then echo There is no hash in this tag.; fi
+    
