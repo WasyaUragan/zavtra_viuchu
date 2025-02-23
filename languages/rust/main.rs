de0426e654f4c32bc3f538c958c67713d8df426b
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-02-23 19:40:23 +0300

    rust

--- languages/rust/main.rs
+++ languages/rust/main.rs
@@ -1,10 +1,16 @@
-// Комментарий.
-fn main() { // Комментарий за главной функцией.
-            /*
-            Многострочный
-            комментарий.
-            */
+use std::io;
+
+fn main() {
+    println!("Пожалуйста, введите свою строку");
+
+    let mut user_input = String::new();
+
+    let result = io::stdin()
+        .read_line(&mut user_input)
+        .expect("Не удалось прочитать ввод");
+
+    println!("Вы ввели: {user_input}");
+    println!("result = {result}");
 }
 
-// отличие метода от функции в rust
 // трейты
