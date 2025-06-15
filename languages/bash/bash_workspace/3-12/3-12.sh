a73755f072da06bf301ba687f2d6b3181f693af7
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-06-15 15:12:57 +0300

    learning bash

--- languages/bash/bash_workspace/3-12/3-12.sh
+++ languages/bash/bash_workspace/3-12/3-12.sh
@@ -8,15 +8,20 @@ declare -i bet=1000
 
 echo "Спорим на $bet деревянных, что не отгадаешь?"
 
+choice() {
 select yn in "Да" "Нет"; do
     case $yn in
         Да ) echo " у Вас есть 7 попыток."; sleep 2s; break;;
         Нет ) exit;;
-    esac
+    esac 
 done
+}
 
 declare -i counter=7
 
+choice
+
+game() {
 for i in {1..7}
 do
     echo "Введите число от 1 до 100 (включительно):"
@@ -24,34 +29,35 @@ do
     echo "Ваш вариант ответа: $otvet"
     sleep 2s
 
-    ((counter=--counter))
+    ((counter=--counter))  
+    
 
-    if (($counter==0)); then
-        sleep 2s
-        echo "ХА!"
-        sleep 2s
-        echo "На пенек сел? $bet должен!"
-        sleep 2s
-        echo "Повысим ставки?"
-        bet=$bet*2 
-        echo "Моя ставка - $bet деревянных"
-        sleep 2s
-        
-        select yn in "Да" "Нет"; do
-            case $yn in
-                Да ) echo " у Вас есть 7 попыток."; sleep 2s;;
-                Нет ) exit;;
-            esac
-        done
-    fi    
-
-    if [[ "$otvet" > "$chislo" ]]; then
-        echo "Вы не угадали. Загаданное число - больше. У Вас осталось $counter попыток."
-    elif [[ "$otvet" < "$chislo" ]]; then
-        echo "Вы не угадали. Загаданное число - меньше. У Вас осталось $counter попыток."
-    #elif [[ "$otvet" == "$chislo" ]]
-    else
+    if (( otvet > chislo )); then
+        echo "Вы не угадали. Загаданное число - меньше."
+    elif (( otvet < chislo )); then
+        echo "Вы не угадали. Загаданное число - больше."
+    elif (( otvet == chislo )); then
         echo "Правильно! Удача на Вашей стороне!"
+        exit 0
     fi
 done
+}
 
+game
+
+while (( counter == 0 )); do
+    sleep 2s; echo "ХА!"
+    sleep 2s; echo "На пенек сел? $bet должен!"
+    sleep 2s; echo "Отыграться хочешь?"
+    sleep 2s; echo "Повысим ставки!"
+    bet=$((bet*2))
+    sleep 1s; echo "Моя ставка - $bet деревянных"
+    sleep 2s; echo "Играем?"
+    counter=7
+    choice
+    chislo=$((1 + $RANDOM % 100))
+    game
+done 
+
+# после победы юзера скрипт хочет отыграться
+# нужно вести счет кто кому сколько должен 
