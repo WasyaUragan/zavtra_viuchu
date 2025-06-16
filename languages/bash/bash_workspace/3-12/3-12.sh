446f08c5b8b9715fe781138e97ebe783bec0c0fd
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-06-16 09:55:27 +0300

    learning bash

--- languages/bash/bash_workspace/3-12/3-12.sh
+++ languages/bash/bash_workspace/3-12/3-12.sh
@@ -1,63 +1,86 @@
 #!/bin/bash
 
-echo "Загадываю число от 1 до 100 включительно..."
-sleep 3s
+message() {
+    echo "========================================"
+    echo "$1"
+    echo "========================================"
+    sleep 2
+}
+
+randomizer() {
+    declare -i chislo=$((1 + $RANDOM % 100))
+}
+
+message "Загадываю число от 1 до 100 включительно..."
 
 declare -i chislo=$((1 + $RANDOM % 100))
 declare -i bet=1000
 
-echo "Спорим на $bet деревянных, что не отгадаешь?"
+message "Спорим на $bet деревянных, что не отгадаешь?"
 
 choice() {
 select yn in "Да" "Нет"; do
     case $yn in
-        Да ) echo " у Вас есть 7 попыток."; sleep 2s; break;;
-        Нет ) exit;;
+        Да)
+            message " у Вас есть 7 попыток."; break;;
+        Нет)
+            bet=$((bet/2))
+            message "Игра завершена. Ваш долг: $bet деревянных"; exit;;
     esac 
 done
 }
 
 declare -i counter=7
+declare -a spisok_otvetov=()
 
 choice
 
 game() {
-for i in {1..7}
-do
-    echo "Введите число от 1 до 100 (включительно):"
-    read otvet
-    echo "Ваш вариант ответа: $otvet"
-    sleep 2s
-
+    for ((i = 7; i > 0; i--))
+    do
+        while true; do
+            read -p "Введите число от 1 до 100 (включительно): " otvet
+            spisok_otvetov+=("$otvet")
+            [[ "$otvet" =~ ^[0-9]+$ ]] && break
+            message "Ошибка: введите корректное число!"
+        done
+    # do
+        # message "Введите число от 1 до 100 (включительно):"
+        # read otvet
+        # message "Ваш вариант ответа: $otvet"
+    
+    spisok_otvetov+=("$otvet")
     ((counter=--counter))  
     
 
     if (( otvet > chislo )); then
-        echo "Вы не угадали. Загаданное число - меньше."
+        message "Вы не угадали. Загаданное число - меньше."
     elif (( otvet < chislo )); then
-        echo "Вы не угадали. Загаданное число - больше."
+        message "Вы не угадали. Загаданное число - больше."
     elif (( otvet == chislo )); then
-        echo "Правильно! Удача на Вашей стороне!"
+        message "Правильно! Удача на Вашей стороне!"
         exit 0
     fi
-done
+    done
 }
 
 game
 
 while (( counter == 0 )); do
-    sleep 2s; echo "ХА!"
-    sleep 2s; echo "На пенек сел? $bet должен!"
-    sleep 2s; echo "Отыграться хочешь?"
-    sleep 2s; echo "Повысим ставки!"
+    message "Вы исчерпали все попытки, Вы проиграли!"
+    message "Хотите Отыграться?"
+    message "Повысим ставки!"
     bet=$((bet*2))
-    sleep 1s; echo "Моя ставка - $bet деревянных"
-    sleep 2s; echo "Играем?"
+    message "Моя ставка - $bet деревянных"
+    message "Играем?"
     counter=7
     choice
-    chislo=$((1 + $RANDOM % 100))
+    randomizer
     game
 done 
 
-# после победы юзера скрипт хочет отыграться
+# 1) добить проверку на числа в диапазоне 1 - 100
+# вести список ответов и проверять, был ли этот вариант ранее
+
+# 2) после победы юзера скрипт хочет отыграться
 # нужно вести счет кто кому сколько должен 
