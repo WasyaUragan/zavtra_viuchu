ef9b0c4d1ddc0e2860fef5f8df39527fff9ce882
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-06-28 13:05:24 +0300

    Refactors bash game and adds heredoc examples
    Updates the bash game script to improve code readability and maintainability by renaming functions to follow a consistent naming convention and using global arrays.
    
    Adds examples of using heredoc in bash scripts to the theory files,
    demonstrating its usefulness for multi-line input and configuration.

--- languages/bash/bash_workspace/3-12/3-12.sh
+++ languages/bash/bash_workspace/3-12/3-12.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-message() {
+Message() {
     echo "========================================"
     echo "$1"
     echo "========================================"
     sleep 2
 }
 
-randomizer() {
+Randomizer() {
     declare -g chislo=$((1 + $RANDOM % 100))
 }
 
-message "Загадываю число от 1 до 100 включительно..."
+Message "Загадываю число от 1 до 100 включительно..."
+Randomizer
 
 declare -i bet=1000
 declare -i round=0
 
 # использую read при простом выборе (Y/N), при нескольких вариантах использую select
-choice() {
+Choice() {
 while true; do    
     read -p "Спорим на $bet деревянных, что не отгадаете? (y/n): " yn  
     case "$yn" in
         y|Y|[Yy][Ee][Ss])
-            message " у Вас есть 3 попытки."
+            Message " у Вас есть 3 попытки."
             break
             ;;
         n|N|[Nn][Oo])
             # ошибка, если отказ при первой итерации :)
             if (( round > 0 )); then
                 bet=$((bet / 2))
-                message "Игра завершена. Ваш долг: $bet деревянных"
+                Message "Игра завершена. Ваш долг: $bet деревянных"
             fi
-            message "Не очень-то и хотелось!"
+            Message "Не очень-то и хотелось!"
             exit
             ;;
         *)
@@ -40,7 +41,7 @@ while true; do
 done
 }
 
-# choice() {
+# Choice() {
 # select yn in "Да" "Нет"; do
 #     case $yn in
 #         ...    
@@ -49,57 +50,59 @@ done
 # }
 
 declare -i counter=3
-declare -a spisok_otvetov=()
+declare -ag SPISOK_OTVETOV=()
 
-choice
+Choice
 
-game() {
+Game() {
     for ((i = 3; i > 0; i--))
     do
         # Валидация числа
         while true; do
             read -p "Введите число от 1 до 100 (включительно): " otvet
             if ! [[ "$otvet" =~ ^[0-9]+$ ]]; then
-                message "Ошибка! Ожидаемый ввод - число в десятичной системе исчисления."
+                Message "Ошибка! Ожидаемый ввод - число в десятичной системе исчисления."
                 continue
             fi
             if ((otvet < 1 || otvet > 100)); then
-                message "Число должно быть от 1 до 100!"
+                Message "Число должно быть от 1 до 100!"
                 continue
             fi
-            spisok_otvetov+=("$otvet")
+            SPISOK_OTVETOV+=("$otvet")
+            round+=1
             break
         done
-    
+
     ((counter=--counter))  
     
     if (( otvet > chislo )); then
-        message "Вы не угадали. Загаданное число - меньше."
+        Message "Вы не угадали. Загаданное число - меньше."
     elif (( otvet < chislo )); then
-        message "Вы не угадали. Загаданное число - больше."
+        Message "Вы не угадали. Загаданное число - больше."
     elif (( otvet == chislo )); then
-        message "Правильно! Удача на Вашей стороне!"
+        Message "Правильно! Удача на Вашей стороне!"
         exit 0
     fi
     done
 }
 
-game
+Game
+
+Message "Список ответов: ${SPISOK_OTVETOV[*]}"
 
 while (( counter == 0 )); do
-    message "Вы исчерпали все попытки, Вы проиграли!"
-    message "Хотите Отыграться?"
-    message "Повысим ставки!"
+    Message "Вы исчерпали все попытки, Вы проиграли!"
     bet=$((bet*2))
-    message "Моя ставка - $bet деревянных"
-    message "Играем?"
+    Message "Моя ставка - $bet деревянных"
+    Message "Играем?"
+    SPISOK_OTVETOV=()
     counter=3
-    choice
-    randomizer
-    game
+    Choice
+    Randomizer
+    Game
 done 
 
-# 1) вести список ответов и проверять, был ли этот вариант ранее
+# 1) список ответов ведется, нужно проверять, был ли этот вариант ранее
 
 # 2) после победы юзера скрипт хочет отыграться
 # нужно вести счет кто кому сколько должен 
