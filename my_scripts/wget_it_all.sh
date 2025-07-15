dad797199440bc294e6c8f584dc67bc8073c17d3
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-07-15 20:43:10 +0300

    Improves package download and management
    Refactors the package download script to prevent redownloading existing packages.
    It also integrates createrepo to update the repository metadata after downloads.
    
    Also, corrects a typo in a note and adds instructions for downloading maven artifacts.

--- my_scripts/wget_it_all.sh
+++ my_scripts/wget_it_all.sh
@@ -2,11 +2,25 @@
 
 file="./wget_list"
 
-pushd "$1" # директория назначения задается параметром $1
-while read line; do
-	echo ===================
-	echo ${line}
-	wget ${line} --no-check-certificate
-	echo ===================
+pushd "$1"  # директория назначения задается параметром $1
+while IFS= read -r pkg; do
+
+        filename="${pkg##*/}" # удаляет самую длинную подстроку до последнего /
+        # awk -F'/' '{print $NF}' ${pkg}
+        # echo ${pkg} | rev | cut -d'/' -f1 | rev
+
+        echo ======================================
+        echo "$pkg"     
+        echo --------------------------------------
+
+        if [[ -f "$filename" ]]; then
+                echo "Пакет $filename уже существует"
+        else
+                echo "Скачиваю пакет $filename"
+                wget ${pkg} --no-check-certificate
+        fi
+
 done < ../${file}
+
+createrepo .
 popd
