b1a56db2a79761b4d8bc904050d43d818e523407
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-07-17 20:13:27 +0300

    learning bash

--- my_scripts/wget_it_all.sh
+++ my_scripts/wget_it_all.sh
@@ -5,7 +5,12 @@ file="./wget_list"
 pushd "$1"  # директория назначения задается параметром $1
 while IFS= read -r pkg; do
 
+        if [[ "$pkg" == "[Pipeline] echo" ]]; then
+                continue
+        fi
+
         filename="${pkg##*/}" # удаляет самую длинную подстроку до последнего /
+        echo "DEBUG: $filename"
         # awk -F'/' '{print $NF}' ${pkg}
         # echo ${pkg} | rev | cut -d'/' -f1 | rev
 
@@ -14,13 +19,16 @@ while IFS= read -r pkg; do
         echo --------------------------------------
 
         if [[ -f "$filename" ]]; then
-            echo "Пакет $filename уже существует"
+                echo "Пакет $filename уже существует"
         else
-            echo "Скачиваю пакет $filename"
-            wget ${pkg} --no-check-certificate
+                echo "Скачиваю пакет $filename"
+                echo
+                wget ${pkg} --no-check-certificate
         fi
 
 done < ../${file}
 
 createrepo .
 popd
+
+# выделить из $filename название без версии - удалить все старые пакеты "rm -rf./название*" перед скачиванием новой
