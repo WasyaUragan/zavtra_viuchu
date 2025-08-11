4c99568844f14be305a43ac376aa82504f938200
Zhuzhukov Ilya Vladlenovich <ilya.zhuzhukov@yandex.ru>
2025-08-11 21:33:22 +0300

    Automates Debian workspace build process
    Automates the creation of a Debian chroot environment for building packages.
    This includes updating the package cache, installing debootstrap, mounting necessary system directories,
    copying DNS configuration, creating a build directory, copying build scripts, making the scripts executable,
    and executing the primary build script within the chroot.

--- languages/workspace/debian/build.sh
+++ languages/workspace/debian/build.sh
@@ -10,28 +10,27 @@ Message "Устанавливаю debootstrap на $HOSTNAME"
 apt install debootstrap -y
 
 Message "Создаю chroot"
-debootstrap bullseye /opt/chroot/ http://deb.debian.org/debian/
+debootstrap --include=vim,dpkg-source-gitarchive bullseye $HOME/chroot/ http://deb.debian.org/debian/
 
-Message "Монтирую /dev в chroot" 
-mount --bind /dev /opt/chroot/dev
-
-Message "Монтирую /proc в chroot"
-mount --bind /proc /opt/chroot/proc
-
-Message "Монтирую /sys в chroot" 
-mount --bind /sys /opt/chroot/sys
+Message "Монтирую /dev /proc /sys в chroot"
+for i in dev proc sys; do
+    mkdir -p $HOME/chroot/$i
+    mount --bind /$i $HOME/chroot/$i
+done
 
 Message "Копирую конфигурацию dns в chroot"
-cp /etc/resolv.conf /opt/chroot/etc/
+cp /etc/resolv.conf $HOME/chroot/etc/
 
 Message "Создаю директорию сборки"
-mkdir /opt/chroot/opt/build_dir
+mkdir -p $HOME/chroot/opt/build_dir
 
-Message "Копирую скрипт в chroot"
-cp build_1.sh /opt/chroot/opt/build_dir
+Message "Копирую необходимые скрипты в chroot"
+cp {build_1.sh,message.sh,color.sh,print_slow_ds.sh} $HOME/chroot/opt/build_dir
 
-Message "Делаю скрипт исполняемым"
-chmod +x /opt/chroot/opt/build_dir/build_1.sh
+Message "Делаю скрипты исполняемыми"
+chmod +x $HOME/chroot/opt/build_dir/{build_1.sh,message.sh,color.sh,print_slow_ds.sh}
 
 Message "Исполняю скрипт внудри chroot"
-chroot /opt/chroot /opt/build_dir/build_1.sh
+chroot $HOME/chroot /opt/build_dir/build_1.sh
+
+#добавить остановку выполнения в случае ошибки
